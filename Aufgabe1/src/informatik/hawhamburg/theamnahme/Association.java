d7a66c723d80d31d09dcfb564bb3b6dd4a1ee46b
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-03-26 19:02:34 +0100

    added gitignore file

---
+++ Aufgabe1/src/informatik/hawhamburg/theamnahme/Association.java
@@ -0,0 +1,11 @@
+package informatik.hawhamburg.theamnahme;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: moritzspindelhirn
+ * Date: 26.03.13
+ * Time: 18:50
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Association {
+}
---
+++ Aufgabe1/src/informatik/hawhamburg/theamnahme/Main.java
@@ -0,0 +1,8 @@
+package informatik.hawhamburg.theamnahme;
+
+public class  Main {
+
+    public static void main(String[] args) {
+
+    }
+}
