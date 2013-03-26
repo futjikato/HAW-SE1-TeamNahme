29fa531032a6bdd4d58dc86f6b49a09bcf046f40
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-03-26 19:26:44 +0100

    added basic example usage

--- Aufgabe1/src/informatik/hawhamburg/theamnahme/Main.java
+++ Aufgabe1/src/informatik/hawhamburg/theamnahme/Main.java
@@ -3,6 +3,20 @@ package informatik.hawhamburg.theamnahme;
 public class  Main {
 
     public static void main(String[] args) {
+        // ad hoc example usage for AssociationList
+        AssociationList<String> stringAssociationList = new AssociationList<String>();
 
+        // add some strings
+        stringAssociationList.add("Object 1");
+        stringAssociationList.add("Object 2");
+        stringAssociationList.add("Object 3");
+
+        try {
+            System.out.println(stringAssociationList.get(1));
+            stringAssociationList.remove(0);
+            System.out.println(stringAssociationList.get(1));
+        } catch (AssociationException e) {
+            e.printStackTrace();
+        }
     }
 }
